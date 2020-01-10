a40e512fdfbc14eed87950cea37cbd99d1a1b67d
Julio Vinachi <julio899@gmail.com>
2020-01-10 07:08:48 -0400

    add tags

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 "use strict";
 
 const log = require("./lib/log");
+const tag = require("./lib/tag");
 const inNumeration = require("./lib/inNumeration");
 
 module.exports = {
   log,
   inNumeration
 };
+
+log(
+	[
+		{
+			text: "Hey import typescript",
+			otro: 1,
+			value: 12324567
+		},
+		{ otro: "mas" },
+		123
+	],
+	inNumeration('US',4567.2)
+);
+tag('test','info');
+tag('uno','info','custon');
+tag('dos','success');
